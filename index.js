a2789d96ab60183a91f5c6a8ef19ffafe3941937
SaintJul <favor.julius@gmail.com>
2021-05-22 02:47:53 -0400

    commit tree 63aa9c31727ee85b50fa88aacb0eb85e08cbe4c1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+function superbowlWin(arry){
+       for (const record of arry){
+           if (record.result === 'W'){
+               return(record.year)
+           }
+       }       
+    
+}
+record.find(superbowlWin)
+//console.log (record.find(element => record.result === 'w'))
+/*for (const result of record){
+    if(result.find(record) === 'W'){
+        return result
+    }
+}*/
